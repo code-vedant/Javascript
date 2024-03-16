714b060e4229431fd612c106a38886222e697e1e
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-16 19:41:30 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)
+
+// Code should be readable
+
+let name = "Batman"  //string
+let age = 18  //number
+let isSleeping = false //boolean
+
+//  number = 2^53 //bigint
+//string = ""
+// Boolean = true/false
+// null = standalone value
+// undefined = value not assigned
+// symbol = unique
+
+
+
+// object
+
+console.log(typeof age);
