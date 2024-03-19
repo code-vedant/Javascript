d9f4183393b9abf9d1594370603cb9accbf2b460
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-19 07:43:58 +0530

    learning operators

---
+++ operators.js
@@ -0,0 +1,30 @@
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+
+let counter = 10
+
+let total = ++counter + counter++
+
+console.log(counter);
+console.log(total);
+
+
+
