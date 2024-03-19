06d569a7d421a8006eac094ea3aa9d76c218f8cb
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-19 07:24:34 +0530

    data conversion

--- datatypes.js
+++ datatypes.js
@@ -20,3 +20,19 @@ let isSleeping = false //boolean
 // object
 
 console.log(typeof age);
+
+let score =null
+
+console.log(score);
+score = 45;
+console.log(score);
+
+let strscore = String(score)
+console.log(typeof score);
+
+console.log(strscore);
+console.log(typeof strscore);
+
+// 1=>> true, 0 =>> false
+//"" => false
+// "kuch bhi" => true
