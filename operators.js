b3468c97ecfc82b743d570c090c8807aefc9f9b4
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-20 06:20:10 +0530

    comparision operators

--- operators.js
+++ operators.js
@@ -17,14 +17,34 @@ let str3 = str1 + str2
 // console.log(1 + "2" + 2);
 // console.log(1 + 2 + "2");
 
-console.log(true);
+// console.log(true);
 
-let counter = 10
+// let counter = 10
 
-let total = ++counter + counter++
+// let total = ++counter + counter++
 
-console.log(counter);
-console.log(total);
+// console.log(counter);
+// console.log(total);
 
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
+// console.log("2" > 1);
+// console.log("02" > 1);
 
+console.log(null > 0);      // avoid
+console.log(null == 0);     // avoid
+console.log(null >= 0);
+
+// comparision function convert null to number
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// == === work differently
+
+// === check data type
