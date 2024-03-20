21dd3d91ef02ecde639325736252c69b06d45e2a
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-20 20:39:57 +0530

    started string

---
+++ strings.js
@@ -0,0 +1,15 @@
+// String
+
+const name = "oonoda"
+const rcCount = 50
+
+console.log(`Hello my name is ${name} and my RC car count is ${rcCount}`);
+
+const myName = new String('ProBot')
+
+console.log(myName[0]);
+console.log(myName.__proto__);
+
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(2));
