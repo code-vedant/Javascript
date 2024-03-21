6ec20c8b16cbb6ddc48b4fc357f1fd35d6c707bd
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-21 07:05:25 +0530

    maths_and_nums

---
+++ nums_and_maths.js
@@ -0,0 +1,37 @@
+const balance = new Number(1000.78578577)
+
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const number = 25.354651
+
+// console.log(number.toPrecision(4));
+
+const rich = 100000000
+// console.log(rich.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.8));
+
+// 4
+// 5
+// 5
+// 4
+
+console.log(Math.min(2,4,3,9,5,3));
+console.log(Math.max(2,4,3,9,5,3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1))+min)); 
