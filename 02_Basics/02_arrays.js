674371815e7544796f53f2805b9a282329a0c6a4
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-03-21 18:34:37 +0530

    array 2

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,21 @@
+// const marvelHero = ['Ironman','Thor','hulk']
+// const dcHero = ['superman','batman','flash']
+
+// const allHeroes = [...marvelHero, ...dcHero]
+
+// console.log(allHeroes);
+
+// const numArr =  [1,2,3,[4,5,6],7,[6,5,[7,6]]]
+
+// const acchaArr = numArr.flat(Infinity)
+// console.log(acchaArr);
+
+console.log(Array.isArray("Hooray"));
+console.log(Array.from("Hooray"));
+console.log(Array.from({name: "Hooray"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of("Score1", "Score2", "Score3"));
